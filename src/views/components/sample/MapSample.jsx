d45dcb3f5738fa0e50d80df9d14198f5dd0f0227
jisup <xrl0603@naver.com>
2022-10-31 16:45:52 +0900

    del : 프로젝트 내 json 데이터 삭제
    모든 geo, topojson은 s3에 저장 후 사용

---
+++ src/views/components/sample/MapSample.jsx
@@ -0,0 +1,76 @@
+import * as d3 from "d3";
+import * as topojson from "topojson";
+
+import { useEffect, useRef } from "react";
+
+import SeoulMin from "assets/json/seoul_min.json";
+
+import "./MapSample.scss";
+function MapSample(props) {
+  var TopoJsonData = SeoulMin;
+
+  var svgRef = useRef(null);
+
+  var width = 700;
+  var height = 700;
+
+  useEffect(() => {
+    const currentElement = svgRef.current;
+
+    const geojson = topojson.feature(
+      TopoJsonData,
+      TopoJsonData.objects.regions
+    );
+    var projection = d3.geoMercator().scale(1).translate([0, 0]);
+
+    const path = d3.geoPath().projection(projection);
+
+    const bounds = path.bounds(geojson);
+    const widthScale = (bounds[1][0] - bounds[0][0]) / width;
+    const heightScale = (bounds[1][1] - bounds[0][1]) / height;
+
+    const center = d3.geoCentroid(geojson);
+
+    const scale = 1 / Math.max(widthScale, heightScale);
+
+    const xoffset =
+      width / 2 - (scale * (bounds[1][0] + bounds[0][0])) / 2 + 10;
+    const yoffset =
+      height / 2 - (scale * (bounds[1][1] + bounds[0][1])) / 2 + 80;
+    const offset = [xoffset, yoffset];
+
+    projection.scale(scale).translate(offset);
+
+    var svg = d3
+      .select(currentElement)
+      .attr("width", width + "px")
+      .attr("height", height + "px");
+    var states = svg.append("g").attr("id", "states");
+
+    // states
+    //   .append("rect")
+    //   .attr("class", "background")
+    //   .attr("width", width + "px")
+    //   .attr("height", height + "px");
+
+    states
+      .selectAll("path")
+      .data(geojson.features)
+      .enter()
+      .append("path")
+      .attr("d", path);
+
+    // d3.json(KoreaGeoJson).then((response) => {
+    //   states
+    //     .selectAll("path")
+    //     .data(response.features)
+    //     .enter()
+    //     .append("path")
+    //     .attr("d", path);
+    // });
+  }, []);
+
+  return <svg ref={svgRef}></svg>;
+}
+
+export default MapSample;
