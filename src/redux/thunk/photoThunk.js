a21a9892c8228a944e83a6c1384a9b069469058b
jisup <xrl0603@naver.com>
2022-11-08 16:29:44 +0900

    add : photo actions set with thunk

--- src/redux/thunk/photoThunk.js
+++ src/redux/thunk/photoThunk.js
@@ -1,5 +1,20 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { addDoc, collection } from "firebase/firestore";
 
 import { db } from "lib/setFilrebase";
 
-export const setPhoto = createAsyncThunk(`photo/setPhoto`, async () => {});
+export const setPhoto = createAsyncThunk(
+  `photo/setPhoto`,
+  async ({ id, name, url, width, height }) => {
+    const data = {
+      album_id: id,
+      name,
+      url,
+      width,
+      height,
+    };
+
+    const docRef = await addDoc(collection(db, "photos"), data);
+    return docRef.id;
+  }
+);
