8ea33913420f5a882d314be2f925263a9090f517
jisup <xrl0603@naver.com>
2022-11-08 20:05:36 +0900

    add : example of S3 file upload
    using S3Client in aws-sdk/client-s3

---
+++ src/views/components/sample/S3UploadSample.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+function S3UploadSample(props) {
+  const [selectFile, setSelectFile] = useState(null);
+
+  const handleFileInput = (e) => {
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target.files);
+    setSelectFile(e.target.files);
+  };
+
+  const handleUpload = async (file) => {
+    const client = new S3Client({
+      region: process.env.REACT_APP_S3_REGION,
+      credentials: {
+        accessKeyId: process.env.REACT_APP_S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.REACT_APP_S3_SECRET_ACCESS_KEY,
+      },
+    });
+
+    // one file
+
+    // const params = {
+    //   Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
+    //   Key: file.name,
+    //   Body: file,
+    // };
+
+    // const command = new PutObjectCommand(params);
+    // const data = await client.send(command);
+    // console.log(data);
+
+    // multi file
+    Object.values(selectFile).map(async (val) => {
+      const params = {
+        Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
+        Key: val.name,
+        Body: val,
+      };
+      console.log(params);
+
+      const command = new PutObjectCommand(params);
+      try {
+        const data = await client.send(command);
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileInput} multiple></input>
+      <button onClick={() => handleUpload(selectFile)}>Upload To S3</button>
+    </div>
+  );
+}
+
+export default S3UploadSample;
