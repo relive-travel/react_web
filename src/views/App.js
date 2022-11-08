2768476a069c9cc7c243f4c4746761bf9c8dfeb9
jisup <xrl0603@naver.com>
2022-11-08 16:30:13 +0900

    del : example, firebase get method, in app js

--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,11 @@
-import "./App.scss";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-import { getCoordinates } from "lib/setFilrebase";
 import { setMapOption, setMapRegion } from "redux/slice/mapSlice";
 
 import D3Map from "views/components/map/D3Map.jsx";
+
+import "./App.scss";
 function App() {
   const dispatch = useDispatch();
 
@@ -20,15 +20,6 @@ function App() {
       setMapOption({ width: window.innerWidth, height: window.innerHeight })
     );
   }, [dispatch]);
-
-  useEffect(() => {
-    getCoordinates().then((response) => {
-      response.map((res) => {
-        console.log(res.latitude);
-        console.log(res.longitude);
-      });
-    });
-  }, []);
 
   return (
     <div className="App">
