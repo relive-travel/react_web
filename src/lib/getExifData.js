65e303a64332c9a5456c9736ddb42cc7dec3e71d
jisup <xrl0603@naver.com>
2022-11-18 18:34:13 +0900

    refactor : div module, setpreview and getexifdata
    - getexifdata : 하나 또는 여러 파일에 대해 동일한 모듈로 진행할 수 있도록 수정 요구

---
+++ src/lib/getExifData.js
@@ -0,0 +1,69 @@
+// var latRef = EXIF.getTag(file, "GPSLatitudeRef");
+// var lat = EXIF.getTag(file, "GPSLatitude");
+// latitude =
+//   latRef === "S"
+//     ? -1 * lat[0] + (60 * lat[1] + -1 * lat[2]) / 3600
+//     : lat[0] + (60 * lat[1] + lat[2]) / 3600;
+// var longRef = EXIF.getTag(file, "GPSLongitudeRef");
+// var long = EXIF.getTag(file, "GPSLongitude");
+// longitude =
+//   longRef === "W"
+//     ? -1 * long[0] + (60 * long[1] + -1 * long[2]) / 3600
+//     : long[0] + (60 * long[1] + long[2]) / 3600;
+// date = EXIF.getTag(file, "DateTime");
+
+import EXIF from "exif-js";
+
+export const getExifData = async (fileObject) => {
+  const [file] = Object.values(fileObject);
+  console.log(file);
+  let exifdata = await new Promise((resolve) => {
+    EXIF.getData(file, () => {
+      var width = EXIF.getTag(file, "ImageWidth");
+      var height = EXIF.getTag(file, "ImageHeight");
+      var lat = EXIF.getTag(file, "GPSLatitude");
+      var latRef = EXIF.getTag(file, "GPSLatitudeRef");
+      var long = EXIF.getTag(file, "GPSLongitude");
+      var longRef = EXIF.getTag(file, "GPSLongitudeRef");
+      var latitude =
+        latRef === "S"
+          ? -1 * lat[0] + (60 * lat[1] + -1 * lat[2]) / 3600
+          : lat[0] + (60 * lat[1] + lat[2]) / 3600;
+      var longitude =
+        longRef === "W"
+          ? -1 * long[0] + (60 * long[1] + -1 * long[2]) / 3600
+          : long[0] + (60 * long[1] + long[2]) / 3600;
+      var dateTime = EXIF.getTag(file, "DateTimeOriginal").split(" ");
+      var date = dateTime[0].replace(/\:/g, "-") + " " + dateTime[1];
+      resolve({
+        width,
+        height,
+        latitude,
+        longitude,
+        date,
+      });
+    });
+  });
+  return {
+    name: file.name,
+    exifdata,
+  };
+};
+
+export const getExifDataList = async (fileObjects) => {
+  const files = Object.values(fileObjects);
+  const filesInfo = Promise.all(
+    files.map(async (file) => {
+      let exifdata = await new Promise((resolve) => {
+        EXIF.getData(file, () => {
+          resolve(EXIF.getAllTags(file));
+        });
+      });
+      return {
+        ...file,
+        exifdata,
+      };
+    })
+  );
+  return filesInfo;
+};
--- src/lib/setPreview.js
+++ src/lib/setPreview.js
@@ -1,5 +1,3 @@
-import EXIF from "exif-js";
-
 import loadImage from "blueimp-load-image";
 
 export const preivewImage = (fileObjects) => {
@@ -27,48 +25,4 @@ export const preivewImage = (fileObjects) => {
 
     reader.readAsDataURL(file);
   });
-};
-
-export const exifImage = async (fileObjects) => {
-  const files = Object.values(fileObjects);
-  const filesInfo = Promise.all(
-    files.map(async (file) => {
-      let exifData = await new Promise((resolve) => {
-        EXIF.getData(file, () => {
-          resolve(EXIF.getAllTags(file));
-        });
-      });
-      return {
-        ...file,
-        exifData,
-      };
-    })
-  );
-  // const filesInfo = files.map((file, index) => {
-  //   var latitude, longitude, date;
-  //   EXIF.getData(file, () => {
-  //     if (EXIF.pretty(file)) {
-  //       var latRef = EXIF.getTag(file, "GPSLatitudeRef");
-  //       var lat = EXIF.getTag(file, "GPSLatitude");
-  //       latitude =
-  //         latRef === "S"
-  //           ? -1 * lat[0] + (60 * lat[1] + -1 * lat[2]) / 3600
-  //           : lat[0] + (60 * lat[1] + lat[2]) / 3600;
-  //       var longRef = EXIF.getTag(file, "GPSLongitudeRef");
-  //       var long = EXIF.getTag(file, "GPSLongitude");
-  //       longitude =
-  //         longRef === "W"
-  //           ? -1 * long[0] + (60 * long[1] + -1 * long[2]) / 3600
-  //           : long[0] + (60 * long[1] + long[2]) / 3600;
-  //       date = EXIF.getTag(file, "DateTime");
-  //     }
-  //   });
-  //   return {
-  //     latitude,
-  //     longitude,
-  //     date,
-  //   };
-  // });
-  console.log(filesInfo);
-  return filesInfo;
 };
