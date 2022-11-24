7f8484c67de9a3c95f74160493decefbdadd3ee5
jisup <xrl0603@naver.com>
2022-11-24 18:25:15 +0900

    add : dialog comp in handAdd comp
    - control the modals, search~, using true & false

--- src/views/components/album/components/HandAdd.jsx
+++ src/views/components/album/components/HandAdd.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback, useRef } from "react";
 import { useSelector } from "react-redux";
 
-import DragAndDrop from "views/common/DragAndDrop";
+import DragAndDrop from "views/components/album/components/components/DragAndDrop";
 import SearchKeyword from "./components/SearchKeyword";
 import SearchRoadAddress from "./components/SearchRoadAddress";
 import SearchLocation from "./components/SearchLocation";
@@ -12,36 +12,28 @@ function HandAdd(props) {
   var addrRef = useRef(null);
   var semiAddrRef = useRef(null);
 
-  const [searchType, setSearchType] = useState(null);
+  const [searchKeyword, setSearchKeyword] = useState(false);
+  const [searchRoadAddress, setSearchRoadAddress] = useState(false);
+  const [searchLocation, setSearchLocation] = useState(false);
 
   const photoFile = useSelector((state) => state.photo.file);
   const photoData = useSelector((state) => state.photo.data);
+
+  const searchData = useSelector((state) => state.album.search);
 
   useEffect(() => {
     console.log(photoData);
   }, [photoData]);
 
   return (
-    <section className="album-auto-info">
+    <section className="album-hand-info">
       <header className="info-header">
         <label htmlFor="title">당근 제목</label>
         <input type="text" id="title"></input>
       </header>
       <main className="info-main">
         <section className="info-main-top">
-          <div className="info-photo">
-            <label htmlFor="photo">
-              <span>*사진</span>
-            </label>
-            <DragAndDrop></DragAndDrop>
-          </div>
-          <div className="info-location">
-            <label htmlFor="location">위치</label>
-            <div className="kakao-map-auto" id="location"></div>
-          </div>
-        </section>
-        <section className="info-main-bottom">
-          <div className="info-date">
+          <article className="info-date">
             <label htmlFor="date">
               <span>*날짜</span>
             </label>
@@ -51,21 +43,44 @@ function HandAdd(props) {
               ref={dateRef}
               readOnly
             ></input>
-          </div>
-          <div className="info-add-address-buttons">
-            <button onClick={() => setSearchType("keyword")}>
-              키워드 검색
+          </article>
+        </section>
+        <section className="info-main-middle">
+          <article className="info-photo">
+            <label htmlFor="photo">
+              <span>*사진</span>
+            </label>
+            <DragAndDrop dragType="hand"></DragAndDrop>
+          </article>
+        </section>
+        <section className="info-main-bottom">
+          <label htmlFor="content">주소 추가</label>
+          <aside className="info-address-buttons">
+            <button onClick={() => setSearchKeyword(true)}>키워드 검색</button>
+            <button onClick={() => setSearchRoadAddress(true)}>
+              도로명 검색
             </button>
-            <button onClick={() => setSearchType("road")}>도로명 검색</button>
-            <button onClick={() => setSearchType("location")}>위치 선택</button>
-          </div>
-          {searchType === "keyword" ? (
-            <SearchKeyword />
-          ) : searchType === "road" ? (
-            <SearchRoadAddress />
-          ) : searchType === "location" ? (
-            <SearchLocation />
+            <button onClick={() => setSearchLocation(true)}>위치 선택</button>
+          </aside>
+          {searchData ? (
+            <section className="search-component">
+              <input
+                type="text"
+                className="search-select"
+                value="선택된정보임"
+              ></input>
+              <input
+                type="text"
+                className="search-optional"
+                value="추가정보입력"
+              ></input>
+            </section>
           ) : null}
+          <section className="search-dialog-component">
+            {searchKeyword ? <SearchKeyword></SearchKeyword> : null}
+            {searchRoadAddress ? <SearchRoadAddress></SearchRoadAddress> : null}
+            {searchLocation ? <SearchLocation></SearchLocation> : null}
+          </section>
         </section>
       </main>
       <footer className="info-footer">
