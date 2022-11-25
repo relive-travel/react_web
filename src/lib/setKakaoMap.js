afce9195e92aee7d9cddd11c4dbbd831560dee74
jisup <xrl0603@naver.com>
2022-11-25 19:25:35 +0900

    add : keyword with kakao map api
    - 키워드를 통해 목록 표출
    - 목록내 정보 표기 및 지도정보 표출

--- src/lib/setKakaoMap.js
+++ src/lib/setKakaoMap.js
@@ -1,5 +1,8 @@
-export const setKakaoMapWithGeoPoint = ({ latitude, longitude }) => {
-  var mapContainer = document.querySelector(".kakao-map-auto");
+export const setKakaoMapWithGeoPoint = ({
+  mapContainer,
+  latitude,
+  longitude,
+}) => {
   var mapOptions = {
     center: new window.kakao.maps.LatLng(latitude, longitude),
     level: 4,
@@ -41,7 +44,107 @@ export const setKakaoMapWithGeoPoint = ({ latitude, longitude }) => {
   });
 };
 
-export const setKakaoMapWithKeyword = (map, latitude, longitude) => {};
+export const setKakaoMapWithKeyword = ({
+  mapContainer,
+  listContainer,
+  pageContainer,
+  keyword,
+}) => {
+  console.log(keyword);
+  var message;
+  var ps = new window.kakao.maps.services.Places();
+
+  const searchPlaces = () => {
+    if (!keyword.replace(/^\s+|\s+$/g, "")) return false;
+    ps.keywordSearch(keyword, (data, status, pagination) => {
+      if (status === window.kakao.maps.services.Status.OK) {
+        displayPlaces(data);
+        displayPagination(pagination);
+      } else if (status === window.kakao.maps.services.Status.ZERO_RESULT) {
+        message = "검색 결과가 존재하지 않습니다.";
+        return;
+      } else if (status === window.kakao.maps.services.Status.ERROR) {
+        message = "검색 결과 중 오류가 발생했습니다.";
+        return;
+      }
+    });
+  };
+  const displayPlaces = (places) => {
+    removeListChild();
+
+    for (let i = 0; i < places.length; i++) {
+      const $article = document.createElement("article");
+      const $button = document.createElement("button");
+
+      $article.innerHTML = `
+          <header class="place-${i}">${i + 1}</header>
+          <main>
+            <section class="place-name">${places[i].place_name}</section>
+            ${
+              places[i].road_address_name
+                ? `<section class="place-road-addr-name">${places[i].road_address_name}</section>
+                <section class="place-addr-name">${places[i].address_name}</section>`
+                : `<section class="place-addr-name">${places[i].address_name}</section>`
+            }
+            </main>
+        `;
+      $button.innerHTML = "보기";
+
+      $button.addEventListener("click", () => {
+        addMarker(new window.kakao.maps.LatLng(places[i].y, places[i].x), i);
+      });
+
+      $article.appendChild($button);
+      listContainer.appendChild($article);
+    }
+  };
+  const displayPagination = (pagination) => {
+    console.log(pagination);
+  };
+  const removeListChild = () => {
+    while (listContainer.firstChild) {
+      if (listContainer.firstChild.tagName === "ARTICLE") {
+        listContainer.removeChild(listContainer.firstChild);
+      } else {
+        break;
+      }
+    }
+  };
+  const addMarker = (position, idx) => {
+    var mapOptions = {
+      center: position,
+      level: 4,
+    };
+    var map = new window.kakao.maps.Map(mapContainer, mapOptions);
+
+    var bounds = new window.kakao.maps.LatLngBounds();
+    bounds.extend(position);
+    map.setBounds(bounds);
+
+    var imgSrc =
+      "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png";
+    var imgSize = new window.kakao.maps.Size(36, 37);
+    var imgOptions = {
+      spriteSize: new window.kakao.maps.Size(36, 691),
+      spriteOrigin: new window.kakao.maps.Point(0, idx * 46 + 10),
+      offset: new window.kakao.maps.Point(13, 37),
+    };
+
+    var markerImg = new window.kakao.maps.MarkerImage(
+      imgSrc,
+      imgSize,
+      imgOptions
+    );
+    var marker = new window.kakao.maps.Marker({
+      position,
+      image: markerImg,
+    });
+
+    marker.setMap(map);
+  };
+
+  searchPlaces();
+};
 
 export const setKakaoMapWithRoad = (map, latitude, longitude) => {};
 
--- src/views/components/album/components/components/SearchKeyword.jsx
+++ src/views/components/album/components/components/SearchKeyword.jsx
@@ -1,30 +1,54 @@
-import { useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
+
+import { setKakaoMapWithKeyword } from "lib/setKakaoMap";
+
 import "./SearchKeyword.scss";
 function SearchKeyword(props) {
-  const keywordRef = useRef(null);
+  const inputRef = useRef(null);
+  const listRef = useRef(null);
+  const pageRef = useRef(null);
+  const compRef = useRef(null);
+  const kakaoMapRef = useRef(null);
 
   const handleSearchClick = (e) => {
-    if (keywordRef.current && !keywordRef.current.contains(e.target)) {
+    if (compRef.current && !compRef.current.contains(e.target)) {
       props.handleSearchClose("roadaddress");
     }
   };
 
+  const handleSetKakaoMap = useCallback(() => {
+    if (inputRef.current.value) {
+      setKakaoMapWithKeyword({
+        mapContainer: kakaoMapRef.current,
+        listContainer: listRef.current,
+        pageContainer: pageRef.current,
+        keyword: inputRef.current.value,
+      });
+    } else {
+      alert("입력해주세요!");
+    }
+  });
+
   return (
     <section className="keyword-component" onClick={handleSearchClick}>
-      <article ref={keywordRef}>
+      <article ref={compRef}>
         <section className="keyword-main">
           <header className="keyword-input">
-            <input type="text"></input>
-            <button>검색</button>
+            <input
+              type="text"
+              ref={inputRef}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") handleSetKakaoMap();
+              }}
+            ></input>
+            <button onClick={handleSetKakaoMap}>검색</button>
           </header>
           <main className="keyword-preview">
-            <section className="keyword-map"></section>
+            <section className="kakao-map-keyword" ref={kakaoMapRef}></section>
           </main>
-          <footer className="keyword-list">
-            <section>
-              <article></article>
-              <button>보기</button>
-            </section>
+          <footer>
+            <section className="keyword-list" ref={listRef}></section>
+            <section className="keyword-pagenation" ref={pageRef}></section>
           </footer>
         </section>
       </article>
