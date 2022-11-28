6d357bc041d21467520bfcdb004c0692884165db
jisup <xrl0603@naver.com>
2022-11-28 18:11:10 +0900

    add : clear button, element ref
    - 각 comp에서 ref를 관리했을 때, 각 데이터가 album-auto-hand comp 서로 상호작용 할수 없었음
    - album에서 auth-hand로 ref를 넘겨 통합적으로 관리하여 상호작용 할 수 있도록 변경

--- src/views/components/album/AlbumDialog.jsx
+++ src/views/components/album/AlbumDialog.jsx
@@ -1,18 +1,53 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import AutoAdd from "./components/AutoAdd";
 import HandAdd from "./components/HandAdd";
 
 import "./AlbumDialog.scss";
 function AlbumDialog(props) {
+  var titleRef = useRef(null);
+  var textRef = useRef(null);
+  var dateRef = useRef(null);
+  var addrRef = useRef(null);
+  var semiAddrRef = useRef(null);
+
   const [albumType, setAlbumType] = useState(props.albumType);
+
+  const handleChangeAlbum = (props) => {};
+
+  const handleAddAlbum = () => {
+    console.log(
+      titleRef.current.value,
+      textRef.current.value,
+      dateRef.current.value,
+      addrRef.current.value,
+      semiAddrRef.current.value
+    );
+  };
+
   return (
     <article className="album-main">
       <header className="album-title">당근 추가 하기</header>
-      <main>{albumType === "auto" ? <AutoAdd /> : <HandAdd />}</main>
+      <main>
+        {albumType === "auto" ? (
+          <AutoAdd
+            titleRef={titleRef}
+            textRef={textRef}
+            dateRef={dateRef}
+            addrRef={addrRef}
+            semiAddrRef={semiAddrRef}
+            handleChangeAlbum={handleChangeAlbum}
+          />
+        ) : (
+          <HandAdd />
+        )}
+      </main>
       <footer className="album-buttons">
+        <button className="album-clear-button">초기화</button>
         <button className="album-preview-button">미리 보기</button>
-        <button className="album-add-button">추가 하기</button>
+        <button className="album-add-button" onClick={handleAddAlbum}>
+          추가 하기
+        </button>
       </footer>
     </article>
   );
