109f4dc5ea58baf11f0638518306630dc84c34b4
jisup <xrl0603@naver.com>
2022-11-28 18:12:35 +0900

    fix : ref
    - ref가 부모 comp에서 관리되어 props.ref로 변경
    - geopoint를 사용한 kakao map에 대한 결과에 따라 주소가 추가되도록 변경

--- src/views/components/album/components/AutoAdd.jsx
+++ src/views/components/album/components/AutoAdd.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
 
 import { setKakaoMapWithGeoPoint } from "lib/setKakaoMap";
@@ -7,73 +7,59 @@ import DragAndDrop from "views/components/album/components/components/DragAndDro
 
 import "./AutoAdd.scss";
 function AutoAdd(props) {
-  var textRef = useRef(null);
-  var dateRef = useRef(null);
-  var addrRef = useRef(null);
-  var semiAddrRef = useRef(null);
   var kakaoMapRef = useRef(null);
-
-  const [photoAddr, setPhotoAddr] = useState(null);
 
   const photoFile = useSelector((state) => state.photo.file);
   const photoData = useSelector((state) => state.photo.data);
 
-  const setInputDate = (exifdate) => {
-    var [date, time] = exifdate.split(" ");
-    time = time.slice(0, time.length - 3);
-    dateRef.current.value = [date, time].join("T");
-  };
-
-  const handleSetKakaoMap = useCallback(async () => {
+  const handleSetKakaoMap = async () => {
     const res = setKakaoMapWithGeoPoint({
       mapContainer: kakaoMapRef.current,
       latitude: photoData.exifdata.latitude,
       longitude: photoData.exifdata.longitude,
     });
-    setPhotoAddr(await res);
-  });
+    const addr = await res;
+    if (addr.road_address) {
+      props.addrRef.current.value = addr.road_address.address_name;
+      props.semiAddrRef.current.value = addr.road_address.building_name;
+    } else {
+      props.addrRef.current.value = addr.address.address_name;
+    }
+  };
 
   const handleResizeHeight = (e) => {
-    console.log(e.target.scrollHeight);
     e.target.style.height = "auto";
     e.target.style.height = e.target.scrollHeight + "px";
   };
 
   useEffect(() => {
     if (photoFile) {
-      console.log(photoFile);
+      if (photoData) {
+        var [date, time] = photoData.exifdata.date.split(" ");
+        time = time.slice(0, time.length - 3);
+        props.dateRef.current.value = [date, time].join("T");
+
+        handleSetKakaoMap();
+      } else {
+        props.handleChangeAlbum();
+      }
     }
   }, [photoFile]);
-
-  useEffect(() => {
-    if (photoData) {
-      setInputDate(photoData.exifdata.date);
-      handleSetKakaoMap();
-    }
-  }, [photoData]);
-
-  useEffect(() => {
-    if (photoAddr) {
-      addrRef.current.value = photoAddr.road_address.address_name;
-      semiAddrRef.current.value = photoAddr.road_address.building_name;
-    }
-  }, [photoAddr]);
 
   return (
     <section className="album-auto-info">
       <header className="info-header">
         <section className="info-title">
           <label htmlFor="title">제목</label>
-          <input type="text" id="title"></input>
+          <input type="text" id="title" ref={props.titleRef}></input>
         </section>
         <section className="info-content">
           <label htmlFor="content">이야기</label>
           <textarea
             id="content"
             rows={1}
-            ref={textRef}
             spellCheck="false"
-            autoCorrect="on"
+            ref={props.textRef}
             onChange={handleResizeHeight}
           ></textarea>
         </section>
@@ -101,29 +87,35 @@ function AutoAdd(props) {
           <section className="info-main-bottom">
             <article className="info-date">
               <label htmlFor="date">
-                <span>*날짜</span>
+                날짜<span>(*)</span>
               </label>
               <input
                 id="date"
                 type="datetime-local"
-                ref={dateRef}
                 readOnly
+                ref={props.dateRef}
               ></input>
             </article>
             <article className="info-address">
               <label htmlFor="address">주소 확인</label>
-              <input id="address" type="text" ref={addrRef}></input>
+              <input
+                id="address"
+                type="text"
+                readOnly
+                ref={props.addrRef}
+              ></input>
             </article>
             <article className="info-semi-address">
-              <label htmlFor="semi-address">추가 주소 정보</label>
-              <input id="semi-address" type="text" ref={semiAddrRef}></input>
+              <label htmlFor="semi-address">추가 정보</label>
+              <input
+                id="semi-address"
+                type="text"
+                ref={props.semiAddrRef}
+              ></input>
             </article>
           </section>
         ) : null}
       </main>
-      {/* <footer className="info-footer">
-
-      </footer> */}
     </section>
   );
 }
