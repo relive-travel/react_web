3f9f23734a45560ab36cd0c86e76f06ef15eb3f8
jisup <xrl0603@naver.com>
2022-12-02 17:00:58 +0900

    add : 앨범 미리보기 추가
    - 기본 가이드라인 샘플 완성

--- src/views/components/modal/AlbumPreview.jsx
+++ src/views/components/modal/AlbumPreview.jsx
@@ -1,13 +1,31 @@
 import "./AlbumPreview.scss";
 function AlbumPreview(props) {
-  const handlePreviewAlbumClose = () => {};
   return (
-    <section className="preview-album-component">
+    <section className="album-preview-component">
       <article>
-        <section className="preview-album-main">
-          <header className="preview-addr"></header>
-          <main className="preview-photos"></main>
-          <footer className="preview-content"></footer>
+        <section className="album-preview-top">
+          <article className="preview-addr">🥕 제목이랍니다</article>
+        </section>
+        <section className="album-preview-bottom">
+          <article className="album-preview-main">
+            <section className="preivew-main-top">
+              <article className="preview-photos"></article>
+            </section>
+            <section className="preview-main-bottom">
+              <article className="preview-writing">
+                <section className="preview-writing-top">
+                  <article className="writing-title">제목</article>
+                  <article className="writing-date">0000-00-00</article>
+                </section>
+                <hr />
+                <section className="preview-writing-bottom">
+                  <article className="writing-content">
+                    내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+                  </article>
+                </section>
+              </article>
+            </section>
+          </article>
         </section>
       </article>
     </section>
