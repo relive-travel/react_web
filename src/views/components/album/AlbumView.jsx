0ee2d941f9171cb1f3061b92e39c4d6f1de568e7
jisup <xrl0603@naver.com>
2022-12-07 20:08:20 +0900

    add : 앨범뷰 데이터 넣기
    - 사진의 경우, InnerHTML로 넣을 수 없기에, 직접 데이터 조절하여 tag로 삽입

--- src/views/components/album/AlbumView.jsx
+++ src/views/components/album/AlbumView.jsx
@@ -1,30 +1,55 @@
-import { useDispatch } from "react-redux";
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import { setAlbumViewDialog } from "redux/slice/statusSlice";
-
+import "swiper/css";
+import "swiper/css/pagination";
 import "./AlbumView.scss";
 function AlbumView(props) {
-  const dispatch = useDispatch();
-
   return (
     <article>
       <section className="album-view-top">
-        <article className="view-addr">하하하</article>
+        <article
+          className="view-addr"
+          title={`🥕 ${props.album.marker.region.addr} ${props.album.marker.region.semiAddr}`}
+        >
+          🥕 {props.album.marker.region.addr}{" "}
+          {props.album.marker.region.semiAddr}
+        </article>
       </section>
       <section className="album-view-bottom">
         <article className="album-view-main">
           <section className="view-main-top">
-            <article className="view-photos"></article>
+            <article className="view-photos">
+              <Swiper modules={[Pagination]} pagination={{ clickable: true }}>
+                {props.album.photo.map((photo, index) => {
+                  const photoStyle =
+                    photo.width > photo.height
+                      ? { height: "100%" }
+                      : { width: "100%" };
+                  return (
+                    <SwiperSlide key={index}>
+                      <img
+                        className={`photo-${index}`}
+                        src={photo.url}
+                        style={photoStyle}
+                      />
+                    </SwiperSlide>
+                  );
+                })}
+              </Swiper>
+            </article>
           </section>
           <section className="view-main-bottom">
             <article className="view-writing">
               <section className="view-writing-top">
-                <article className="writing-title">하하하</article>
-                <article className="writing-date">하하하</article>
+                <article className="writing-title">{props.album.title}</article>
+                <article className="writing-date">{props.album.date}</article>
               </section>
               <hr />
               <section className="view-writing-bottom">
-                <article className="writing-content">하하하</article>
+                <article className="writing-content">
+                  {props.album.content}
+                </article>
               </section>
             </article>
           </section>
