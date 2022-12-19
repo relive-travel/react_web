5972f82fd2fffe6fcdaf61a8e31c92da52ce4660
jisup <xrl0603@naver.com>
2022-12-19 22:55:39 +0900

    add : redux, photo에 all, gather 추가
    - all : album gather comp에서 유저의 모든 photo를 가져온 결과값
    - gather : sort 설정값에 따라 photo에 album 또는 marker 정보를 추가한 결과값

--- src/redux/slice/photoSlice.js
+++ src/redux/slice/photoSlice.js
@@ -1,8 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getPhotoAll } from "redux/thunk/photoThunk";
 
 const initialState = {
-  file: null,
-  data: null,
+  file: null, //drag and drop comp에서 input file데이터
+  data: null, //drag and drop comp에서 input file의 exif데이터
+  all: null, //album all comp에서 사용될 모든 데이터
+  gather: null, //album gather comp에서 사용될 병합 데이터
 };
 
 const photoSlice = createSlice({
@@ -19,6 +22,11 @@ const photoSlice = createSlice({
       state.file = null;
       state.data = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getPhotoAll.fulfilled, (state, action) => {
+      state.all = action.payload;
+    });
   },
 });
 
