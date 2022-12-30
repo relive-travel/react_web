7adbb86eda6736bc907d50798c770d0d98d63287
jisup <xrl0603@naver.com>
2022-12-31 01:54:24 +0900

    add : s3 객체 관련 CRU 추가
    - (Create) setUserObject : User의 key를 가지는 folder 또는 object 추가
    - (Read) getUserObjectList : 객체에서 특정 key를 가지는 object list를 반환
    - (Update) copyUserObjectList : 회원의 정보가 변경되는 경우 객체 Object list를 복사

--- src/lib/utils/s3Utils.js
+++ src/lib/utils/s3Utils.js
@@ -1,5 +1,61 @@
-import { client } from "lib/set/s3Client";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { s3Client } from "lib/set/s3Client";
+import {
+  PutObjectCommand,
+  ListObjectsCommand,
+  CopyObjectCommand,
+} from "@aws-sdk/client-s3";
+
+export const setUserObject = async ({ email, key }) => {
+  const params = {
+    Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
+    Key: `photo/${email}/${key}`,
+  };
+  const command = new PutObjectCommand(params);
+  try {
+    const data = await s3Client.send(command);
+    return data.$metadata.httpStatusCode === 200;
+  } catch (err) {
+    throw Error(err.message);
+  }
+};
+
+export const getUserObjectList = async ({ email }) => {
+  const params = {
+    Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/interfaces/listobjectsv2request.html
+    Prefix: `photo/${email}/`,
+  };
+  const command = new ListObjectsCommand(params);
+  try {
+    const data = await s3Client.send(command);
+    return data;
+  } catch (err) {
+    throw Error(err.message);
+  }
+};
+
+export const copyUserObjectList = async ({ email, newEmail }) => {
+  const copyObjects = await getUserObjectList({ email });
+
+  copyObjects.Contents.slice(1).forEach(async (object) => {
+    const [key] = object.Key.split("/").slice(-1);
+    const params = {
+      Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
+      // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/interfaces/copyobjectrequest.html
+      CopySource: encodeURI(
+        `${process.env.REACT_APP_S3_BUCKET_NAME}/${object.Key}`
+      ),
+      Key: `photo/${newEmail}/${key}`,
+    };
+    const command = new CopyObjectCommand(params);
+    try {
+      const data = await s3Client.send(command);
+      return data;
+    } catch (err) {
+      throw Error(err.message);
+    }
+  });
+};
 
 export const uploadFiles = async ({ files, title, email }) => {
   const filesInfo = Promise.all(
@@ -12,7 +68,7 @@ export const uploadFiles = async ({ files, title, email }) => {
       };
       const command = new PutObjectCommand(params);
       try {
-        const data = await client.send(command);
+        const data = await s3Client.send(command);
         if (data.$metadata.httpStatusCode === 200) {
           return {
             name: `${title}-${index}`,
@@ -21,8 +77,8 @@ export const uploadFiles = async ({ files, title, email }) => {
         } else {
           return null;
         }
-      } catch (e) {
-        throw Error(e.message);
+      } catch (err) {
+        throw Error(err.message);
       }
     })
   );
