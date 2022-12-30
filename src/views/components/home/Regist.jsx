76e8fe0bfa9ec9e02c6e7756455d4df8775ec0d4
jisup <xrl0603@naver.com>
2022-12-30 20:13:10 +0900

    add : regist comp 내용 요소 추가
    - login 후 저장된 user의 nickName, email을 보여줌
    - 각 내용은 변경할 수 없게 설정
    - 버튼 로직 추후 구현

--- src/views/components/home/Regist.jsx
+++ src/views/components/home/Regist.jsx
@@ -5,16 +5,22 @@ function Regist() {
   const userEmail = useSelector((state) => state.user.email);
 
   return (
-    <section className="register-component">
+    <section className="regist-component">
       <header>회원 가입 정보</header>
       <main>
-        <section className="register-notify">
-          정보를 바꾸면 토끼가 알아볼 수 없어요!
-        </section>
-        <section className="register-buttons">
-          <button className="register-rallback-button">돌아 가기</button>
-          <button className="register-complete-button">회원 가입</button>
-        </section>
+        <header>정보가 바뀌면 토끼가 알아볼 수 없어요!</header>
+        <main className="regist-info">
+          <article className="regist-nickName">
+            닉네임 : <span>{userNickName}</span>
+          </article>
+          <article className="regist-email">
+            이메일 : <span>{userEmail}</span>
+          </article>
+        </main>
+        <footer className="regist-buttons">
+          <button className="regist-rallback-button">돌아 가기</button>
+          <button className="regist-complete-button">회원 가입</button>
+        </footer>
       </main>
       <footer>※ 카카오에서 동의된 닉네임, 이메일을 사용합니다</footer>
     </section>
