dbc92f55534f4fe0577d32ab0931bce26e298870
jisup <xrl0603@naver.com>
2022-12-31 01:51:00 +0900

    refactor : s3, rename module
    - client to s3Client : 무엇의 client인지 명확하게 인지

--- src/lib/set/s3Client.js
+++ src/lib/set/s3Client.js
@@ -1,6 +1,6 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-export const client = new S3Client({
+export const s3Client = new S3Client({
   region: process.env.REACT_APP_S3_REGION,
   credentials: {
     accessKeyId: process.env.REACT_APP_S3_ACCESS_KEY_ID,
