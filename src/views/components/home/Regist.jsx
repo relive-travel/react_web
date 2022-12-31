262e037bd5e5635f164df678f181dbca33feb37a
jisup <xrl0603@naver.com>
2022-12-31 19:59:58 +0900

    add : kakao client secret 기능 활성화
    - REST API요청 시 client secret 기능 활성화에 따른 client_secret 값 추가 전송

--- src/views/components/home/Regist.jsx
+++ src/views/components/home/Regist.jsx
@@ -1,8 +1,41 @@
-import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { setUser } from "redux/thunk/userThunk";
+
+import { delCookie } from "lib/utils/cookie";
+import {
+  setPhotoUserFolder,
+  getPhotoUserFolder,
+  copyPhotoUserFolder,
+} from "lib/utils/s3Utils";
 
 function Regist() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userKakaoId = useSelector((state) => state.user.kakaoId);
   const userNickName = useSelector((state) => state.user.nickName);
   const userEmail = useSelector((state) => state.user.email);
+
+  const handleRallbackClick = () => {
+    delCookie({ name: "authorize-access-token" });
+    delCookie({ name: "authorize-refresh-token" });
+    // navigate("/")
+    navigate("/login");
+  };
+
+  const handleCompleteClick = async () => {
+    // dispatch(
+    //   setUser({
+    //     kakaoId: userKakaoId,
+    //     nickName: userNickName,
+    //     email: userEmail,
+    //   })
+    // );
+    // setPhotoUserFolder({ email: userEmail });
+    // navigate("/success");
+  };
 
   return (
     <section className="regist-component">
@@ -18,8 +51,18 @@ function Regist() {
           </article>
         </main>
         <footer className="regist-buttons">
-          <button className="regist-rallback-button">돌아 가기</button>
-          <button className="regist-complete-button">회원 가입</button>
+          <button
+            className="regist-rallback-button"
+            onClick={handleRallbackClick}
+          >
+            돌아 가기
+          </button>
+          <button
+            className="regist-complete-button"
+            onClick={handleCompleteClick}
+          >
+            회원 가입
+          </button>
         </footer>
       </main>
       <footer>※ 카카오에서 동의된 닉네임, 이메일을 사용합니다</footer>
