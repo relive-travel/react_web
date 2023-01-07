561eeab406c0025d4483c9008f9ab34dd6b969ad
jisup <xrl0603@naver.com>
2023-01-07 18:06:38 +0900

    fix : file exifData 올바르지 않는 이미지 크기 수정
    - exif-js의 Image Width와 Height는 Orient가 추가 제공되어 이들의 관계를 해석하여 실크기를 얻어야한다.
    - 현재 이미지의 정확한 사이즈를 위해 loadImage를 사용하고 있었으며, 적용되지 않던 부분을 수정하였음.

--- src/lib/utils/data/exifData.js
+++ src/lib/utils/data/exifData.js
@@ -1,29 +1,31 @@
 import EXIF from "exif-js";
+import loadImage from "blueimp-load-image";
 
 export const getExifData = async (fileObject) => {
   const [file] = Object.values(fileObject);
   let exifdata = await new Promise((resolve) => {
-    EXIF.getData(file, () => {
+    EXIF.getData(file, async () => {
       if (EXIF.pretty(file)) {
-        var width = EXIF.getTag(file, "ImageWidth");
-        var height = EXIF.getTag(file, "ImageHeight");
-        var lat = EXIF.getTag(file, "GPSLatitude");
-        var latRef = EXIF.getTag(file, "GPSLatitudeRef");
-        var long = EXIF.getTag(file, "GPSLongitude");
-        var longRef = EXIF.getTag(file, "GPSLongitudeRef");
+        const fileExifData = await loadImage(file);
+        const width = fileExifData.originalWidth;
+        const height = fileExifData.originalHeight;
+        let lat = EXIF.getTag(file, "GPSLatitude");
+        let latRef = EXIF.getTag(file, "GPSLatitudeRef");
+        let long = EXIF.getTag(file, "GPSLongitude");
+        let longRef = EXIF.getTag(file, "GPSLongitudeRef");
         if (!lat && !long) {
           resolve(null);
         } else {
-          var latitude =
+          let latitude =
             latRef === "S"
               ? -1 * lat[0] + (60 * lat[1] + -1 * lat[2]) / 3600
               : lat[0] + (60 * lat[1] + lat[2]) / 3600;
-          var longitude =
+          let longitude =
             longRef === "W"
               ? -1 * long[0] + (60 * long[1] + -1 * long[2]) / 3600
               : long[0] + (60 * long[1] + long[2]) / 3600;
-          var dateTime = EXIF.getTag(file, "DateTimeOriginal").split(" ");
-          var date = dateTime[0].replace(/\:/g, "-") + " " + dateTime[1];
+          let dateTime = EXIF.getTag(file, "DateTimeOriginal").split(" ");
+          let date = dateTime[0].replace(/\:/g, "-") + " " + dateTime[1];
           resolve({
             width,
             height,
@@ -48,8 +50,15 @@ export const getExifDataList = async (fileObjects) => {
   const filesInfo = Promise.all(
     files.map(async (file) => {
       let exifdata = await new Promise((resolve) => {
-        EXIF.getData(file, () => {
-          resolve(EXIF.getAllTags(file));
+        EXIF.getData(file, async () => {
+          const fileOriginal = await loadImage(file);
+          const width = fileOriginal.originalWidth;
+          const height = fileOriginal.originalHeight;
+
+          let fileExifData = EXIF.getAllTags(file);
+          fileExifData.ImageWidth = width;
+          fileExifData.ImageHeight = height;
+          resolve(fileExifData);
         });
       });
       return {
